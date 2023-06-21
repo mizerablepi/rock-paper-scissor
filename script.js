7462110a9c93f85b0c8c47fa314bf3c08398f7f4
miz <mizerablepi@gmail.com>
2023-06-21 19:14:51 +0530

    complete the js logic

---
+++ script.js
@@ -0,0 +1,75 @@
+function getComputerChoice() {
+  let num =Math.round(Math.random() * 100);
+  if (num % 3 == 0) {
+    return 'rock'
+  } else if (num % 3 == 1) {
+    return 'paper'
+  } else {
+    return 'scissor'
+  }
+}
+
+function isValidOption(choice) {
+  if (choice.toLowerCase() == 'rock' || choice.toLowerCase() == 'paper' || choice.toLowerCase() == 'scissor') {
+    return true; 
+  }
+  return false;
+}
+
+function playRound(computerChoice, playerChoice) {
+  playerChoice = playerChoice.toLowerCase();
+  
+  if (computerChoice == playerChoice) {
+    return 0.5;
+  } else if (computerChoice == 'rock') {
+    if (playerChoice == 'scissor') {
+      return 0;
+    } else {
+      return 1;
+    }
+  } else if (computerChoice == 'paper') {
+    if (playerChoice == 'rock') {
+      return 0;
+    } else {
+      return 1;
+    }
+  } else if (computerChoice == 'scissor') {
+    if (playerChoice == 'paper') {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
+}
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  for (i = 0; i < 5; i++){
+    const playerChoice = prompt('enter your choice');
+    if (!isValidOption(playerChoice)) {
+      console.log("i don't play with people that can't type");
+      return;
+    }
+    const computerChoice = getComputerChoice();
+    result = playRound(computerChoice, playerChoice);
+    playerScore += result;
+    if (result == 1) {
+      console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+    } else if (result == 0) {
+      console.log(`You Lose! ${computerChoice} beats ${playerChoice}`);
+    } else {
+      console.log('draw!')
+    }
+  }
+  computerScore = 5 - playerScore;
+  if (playerScore > computerScore) {
+    console.log('player wins')
+  } else if (playerScore < computerScore) {
+    console.log('computer wins')
+  } else {
+    console.log('draw! no one wins')
+  }
+}
+
+game()
